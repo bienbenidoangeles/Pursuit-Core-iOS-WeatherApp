6aeadb254660b0fc45686ca97f4b11ef6b153cfb
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2020-01-31 15:15:45 -0500

    Added Files

---
+++ WeatherApp/WeatherApp/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  WeatherApp
+//
+//  Created by Bienbenido Angeles on 1/31/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets {
+    public static let apiKey = ""
+}
---
+++ WeatherApp/WeatherApp/WeatherTabBarControllerViewController.swift
@@ -0,0 +1,37 @@
+//
+//  WeatherTabBarControllerViewController.swift
+//  WeatherApp
+//
+//  Created by Bienbenido Angeles on 1/31/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTabBarControllerViewController: UITabBarController {
+    
+    private lazy var navController: UINavigationController = {
+        let navVC = UINavigationController(rootViewController: <#T##UIViewController#>)
+    }()
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
